0519f65562e0bd8e6ea2b490713515e3074b1338
Pierre <pierre.dalbianco@free.fr>
2019-01-27 15:33:05 +0000

    Replace socket.cpp

--- Resize/socket.cpp
+++ Resize/socket.cpp
@@ -6,11 +6,11 @@ TcpClient::TcpClient(QObject *parent) :
 }
 TcpClient::~TcpClient()
 {
-    if(socket != nullptr)
+    /*if(socket != nullptr)
     {
         socket->close();
         delete socket;
-    }
+    }*/
 }
 
 void TcpClient::doSend(const QString &hostIP, quint16 port, const QString &msg)
@@ -18,18 +18,27 @@ void TcpClient::doSend(const QString &hostIP, quint16 port, const QString &msg)
     socket = new QTcpSocket(this);
     message = msg;
 
-    connect(socket, SIGNAL(connected()),this, SLOT(connectedSuccess()));
-    connect(socket, SIGNAL(disconnected()),this, SLOT(disconnected()));
-    connect(socket, SIGNAL(bytesWritten(qint64)),this, SLOT(bytesWritten(qint64)));
+    connect(socket, &QTcpSocket::connected,this, &TcpClient::connectedSuccess);
+    connect(socket, &QTcpSocket::disconnected,this, &TcpClient::disconnected);
+    connect(socket, &QTcpSocket::bytesWritten,this, &TcpClient::bytesWritten);
+    connect(socket, QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::error),this, &TcpClient::errorDetected);
 
     // this is not blocking call
     socket->connectToHost(hostIP, port);
+    socket->waitForConnected(10000);
 
     // we need to wait...
-    if(socket->waitForConnected(10000))
+    /*if(!socket->waitForConnected(10000))//10000
     {
         std::cerr << "Error: " << socket->errorString().toStdString() << std::endl;
-    }
+        emit finished();
+    }*/
+}
+
+void TcpClient::errorDetected()
+{
+    std::cerr << "Error: " << socket->errorString().toStdString() << std::endl;
+    //emit finished();
 }
 
 void TcpClient::connectedSuccess()
@@ -38,12 +47,12 @@ void TcpClient::connectedSuccess()
     socket->write(message.toStdString().c_str());
     socket->waitForBytesWritten(5000);
 }
-
 void TcpClient::disconnected()
 {
     std::cout << "Disconnected..." << std::endl;
     socket->flush();
     socket->close();
+    //emit finished();
 }
 
 void TcpClient::bytesWritten(qint64 bytes)

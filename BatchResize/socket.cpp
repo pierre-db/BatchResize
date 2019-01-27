7af45c261b60ffea24655afa7e31fefbe9cc30fd
Pierre <pierre.dalbianco@free.fr>
2019-01-27 15:28:28 +0000

    Replace socket.cpp

--- BatchResize/socket.cpp
+++ BatchResize/socket.cpp
@@ -1,16 +1,18 @@
 #include "socket.h"
 
+///////////////////////
+//Client
 TcpClient::TcpClient(QObject *parent) :
     QObject(parent)
 {
 }
 TcpClient::~TcpClient()
 {
-    if(socket != nullptr)
+    /*if(socket != nullptr)
     {
         socket->close();
         delete socket;
-    }
+    }*/
 }
 
 void TcpClient::doSend(const QString &hostIP, quint16 port, const QString &msg)
@@ -18,18 +20,27 @@ void TcpClient::doSend(const QString &hostIP, quint16 port, const QString &msg)
     socket = new QTcpSocket(this);
     message = msg;
 
-    connect(socket, SIGNAL(connected()),this, SLOT(connectedSuccess()));
-    connect(socket, SIGNAL(disconnected()),this, SLOT(disconnected()));
-    connect(socket, SIGNAL(bytesWritten(qint64)),this, SLOT(bytesWritten(qint64)));
+    connect(socket, &QTcpSocket::connected,this, &TcpClient::connectedSuccess);
+    connect(socket, &QTcpSocket::disconnected,this, &TcpClient::disconnected);
+    connect(socket, &QTcpSocket::bytesWritten,this, &TcpClient::bytesWritten);
+    connect(socket, QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::error),this, &TcpClient::errorDetected);
 
     // this is not blocking call
     socket->connectToHost(hostIP, port);
+    socket->waitForConnected(10000);
 
     // we need to wait...
-    if(!socket->waitForConnected(5000))
+    /*if(!socket->waitForConnected(10000))//10000
     {
         std::cerr << "Error: " << socket->errorString().toStdString() << std::endl;
-    }
+        emit finished();
+    }*/
+}
+
+void TcpClient::errorDetected()
+{
+    std::cerr << "Error: " << socket->errorString().toStdString() << std::endl;
+    //emit finished();
 }
 
 void TcpClient::connectedSuccess()
@@ -38,24 +49,27 @@ void TcpClient::connectedSuccess()
     socket->write(message.toStdString().c_str());
     socket->waitForBytesWritten(5000);
 }
-
 void TcpClient::disconnected()
 {
     std::cout << "Disconnected..." << std::endl;
     socket->flush();
     socket->close();
+    //emit finished();
 }
 
 void TcpClient::bytesWritten(qint64 bytes)
 {
-    std::cout << bytes << " bytes written..."  << std::endl;
+    //std::cout << bytes << " bytes written..."  << std::endl;
     socket->flush();
     socket->close();
 }
 
+/////////////////////////////
+//Server
 TcpServer::TcpServer(QObject *parent) :
     QObject(parent)
 {
+    socket = nullptr; // need to explicitly declare it to avoid crash on destruction
     server = new QTcpServer(this);
     connect(server, SIGNAL(newConnection()), this, SLOT(newConnection()));
 }
@@ -64,12 +78,12 @@ TcpServer::~TcpServer()
 {
     if(socket != nullptr)
     {
-        socket->close();//necessary?
+        //socket->close();//necessary?
         delete socket;
     }
     if(server != nullptr)
     {
-        server->close();
+        //server->close();
         delete server;
     }
 }
@@ -82,10 +96,33 @@ void TcpServer::startServer(const QString &ipConnect, quint16 port)
     if(!server->listen(ipAddress, port))
     {
         std::cerr << "Server could not start!" << std::endl;
+        emit serverFailStart();
     }
     else
     {
         std::cout << "Server on" << std::endl;
+    }
+}
+
+void TcpServer::restartServer(const QString &ipConnect, quint16 port)
+{
+    server->close();
+    if(socket != nullptr)
+    {
+        socket->flush();
+        socket->close();
+    }
+    QHostAddress ipAddress;
+    ipAddress.setAddress(ipConnect);//QHostAddress::LocalHost
+
+    if(!server->listen(ipAddress, port))
+    {
+        std::cerr << "Server could not restart!" << std::endl;
+        emit serverFailStart();
+    }
+    else
+    {
+        std::cout << "Server restarted" << std::endl;
     }
 }
 

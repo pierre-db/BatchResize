808020084cbd35c40513e03f8d6ce3fb16fc75ac
Pierre <pierre.dalbianco@free.fr>
2019-01-27 15:26:17 +0000

    Replace resizesettings.cpp

--- BatchResize/resizesettings.cpp
+++ BatchResize/resizesettings.cpp
@@ -19,16 +19,84 @@ ResizeSettings::ResizeSettings()
          port = (quint16)s->value("port").toInt();
 }
 
-void ResizeSettings::magickSetup(const QString& mpath, const QString& margs)
+void ResizeSettings::magickSetup(const QString& defaultMagickpath, const QString& defaultArgs, const QString& defaultOutputfolder)
 {
     if(!s->contains("magickpath"))
-        s->setValue("magickpath", mpath);
+        s->setValue("magickpath", defaultMagickpath);
     if(!s->contains("args"))
-        s->setValue("args", margs);
+        s->setValue("args", defaultArgs);
 
-    magickpath = s->value("magickpath").toString();
-    args = s->value("args").toString();
+    if(!s->contains("outputfolderon"))
+        s->setValue("outputfolderon", false);
+    if(!s->contains("outputfolder"))
+        s->setValue("outputfolder", defaultOutputfolder);
 
+    magickpath = s->value("magickpath").toString().trimmed();
+    args = s->value("args").toString().simplified();
+
+    outputfolder = s->value("outputfolder").toString().trimmed();
+    outputfolderon = s->value("outputfolderon").toBool();
+}
+
+void ResizeSettings::iconsSetup(const QString& ipath)
+{
+    if(!s->contains("iconspath"))
+        s->setValue("iconspath", ipath);
+
+    iconspath = s->value("iconspath").toString().trimmed();
+	
+	if(!s->contains("iconssize"))
+        s->setValue("iconssize", "16");
+	
+	iconssize = s->value("iconssize").toInt();
+
+    if(iconssize != 16 && iconssize != 24 && iconssize != 32)
+	{
+        iconssize = 24;
+        s->setValue("iconssize", "24");
+	}
+
+}
+
+void ResizeSettings::simultaneousSetup(const bool state)
+{
+    if(!s->contains("simultaneous"))
+        s->setValue("simultaneous", state);
+
+    simultaneous = s->value("simultaneous").toBool();
+}
+
+void ResizeSettings::toggleSimultaneous()
+{
+    simultaneous = !simultaneous;
+    s->setValue("simultaneous", simultaneous);
+}
+
+void ResizeSettings::saveSettings(const QMap<QString, QString>& newsettings)
+{
+    args = newsettings.value("args");
+    s->setValue("args", args);
+
+    magickpath = newsettings.value("magickpath");
+    s->setValue("magickpath", magickpath);
+
+    outputfolder = newsettings.value("outputfolder");
+    s->setValue("outputfolder", outputfolder);
+
+    outputfolderon = newsettings.value("outputfolderon").toInt();
+    s->setValue("outputfolderon", outputfolderon);
+
+    iconspath = newsettings.value("iconspath");
+    s->setValue("iconspath", iconspath);
+
+    iconssize = newsettings.value("iconssize").toInt();
+    s->setValue("iconssize", iconssize);
+
+    ipaddress = newsettings.value("ipaddress");
+    s->setValue("ipaddress", ipaddress);
+
+    port = newsettings.value("port").toInt();
+    s->setValue("port", port);
 }
 
 ResizeSettings::~ResizeSettings()
